4636c229a96e2a7cfea63064bdd33689e7f9fb23
Nicolas Eeckeloo <neeckeloo@gmail.com>
2012-10-23 07:43:03 +0200

    Create compiler abstract class

---
+++ src/Closure/AbstractCompiler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ClosureCompilerPHP
+ *
+ * @link      http://github.com/neeckeloo/ClosureCompilerPHP
+ * @copyright Copyright (c) 2012 Nicolas Eeckeloo
+ */
+namespace Closure;
+
+abstract class AbstractCompiler implements Compiler\CompilerInterface
+{
+    public function compile()
+    {
+        
+    }
+}
